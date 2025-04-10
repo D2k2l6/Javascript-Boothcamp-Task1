4fc2ee7bd4e27668c8715bafb6726a542d7b38d4
D2k2l6 <142404245+D2k2l6@users.noreply.github.com>
2025-04-10 18:56:56 +0200

    Exercise 1: Variable Declaration

--- variables.js
+++ variables.js
@@ -2,6 +2,6 @@ let name = "Ndivhuwo";
 let age = 25;
 let isStudent = true;
 
-console.log("Name:");
-console.log("Age:");
-console.log("Is Student:");
+console.log("Name:" name);
+console.log("Age:" age);
+console.log("Is Student:" isStudent);
