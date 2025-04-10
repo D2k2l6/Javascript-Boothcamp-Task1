d3bcde323542f71ab41e946bda3a271ba7cd6b54
D2k2l6 <142404245+D2k2l6@users.noreply.github.com>
2025-04-10 12:06:33 +0200

    Delete variables.js

--- variables.js
+++
@@ -1,6 +0,0 @@
-let favoriteColour = "Blue";
-console.log("Favorite Colour:");
-
-// Reassign favoriteColour to a different color
-favoriteColour = "Green";
-console.log("Updated Favorite Colour:");
