35d573ca4f46ed4740bccc91c3d1a864cea2011b
D2k2l6 <142404245+D2k2l6@users.noreply.github.com>
2025-04-10 18:09:38 +0200

    Exercise 2: Variable

---
+++ variable.js
@@ -0,0 +1,6 @@
+let favoriteColour = "Blue";
+console.log("Favorite Colour:");
+
+// Reassign favoriteColour to a different color
+favoriteColour = "Green";
+console.log("Updated Favorite Colour:");
